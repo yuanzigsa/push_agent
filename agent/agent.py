6e5d1b92a96c46a83e08f5e138c8ec3580d19c11
yzgsa <8397701+yzgsa@user.noreply.gitee.com>
2024-07-08 17:57:12 +0800

    time

--- agent/agent.py
+++ agent/agent.py
@@ -39,7 +39,7 @@ class Agent:
         threading.Thread(target=self.collect_task, daemon=True).start()
         self.collect_event.wait()  # 等待 collect_task 完成第一次任务
         self.logger.info("流量采集线程已启动！")
-        threading.Thread(target=self.sync_task, daemon=True).start()
+        # threading.Thread(target=self.sync_task, daemon=True).start()
         self.logger.info("平台同步线程已启动！")
         self.sync_event.wait()  # 等待 sync_task 完成第一次任务
         threading.Thread(target=self.push_task, daemon=True).start()
@@ -47,26 +47,15 @@ class Agent:
 
     def collect_task(self):
         while True:
-
             info = self.hardware_collector.collect_netifio()
-
             with self.shared_info.lock:
                 self.shared_info.info.append(info)
-
-            if info:
-                self.logger.info("流量信息采集成功")
-                self.logger.info(f"第【{len(self.shared_info.info)}/{self.total}】轮采集完成")
-            else:
-                self.logger.error("第【{len(self.shared_info.info)}/{total}】轮采集失败")
-
-            if not self.collect_event.is_set():
-                self.collect_event.set()  # 第一次任务完成后设置事件
-            time.sleep(self.collect_interval)
-
-    def sync_task(self):
-        while True:
-            # 推送信息、从服务器获取配置并进行比对
-            with self.shared_info.lock:
+                if info:
+                    self.logger.info("流量信息采集成功")
+                    self.logger.info(f"第【{len(self.shared_info.info)}/{self.total}】轮采集完成")
+                else:
+                    self.logger.error("第【{len(self.shared_info.info)}/{total}】轮采集失败")
+                # with self.shared_info.lock:
                 info = self.shared_info.info
                 success = self.server_sync.sync(info, self.shared_info.machine_config)
                 if success:
@@ -75,9 +64,20 @@ class Agent:
                         self.shared_info.machine_config = success
                 else:
                     self.logger.error("平台信息同步失败")
-            if not self.sync_event.is_set():
-                self.sync_event.set()  # 第一次任务完成后设置事件
-            time.sleep(self.sync_interval)
+            time.sleep(self.collect_interval)
+
+    # def sync_task(self):
+    #     while True:
+    #         # 推送信息、从服务器获取配置并进行比对
+    #         with self.shared_info.lock:
+    #             info = self.shared_info.info
+    #             success = self.server_sync.sync(info, self.shared_info.machine_config)
+    #             if success:
+    #                 self.logger.info("平台信息同步成功")
+    #                 if self.shared_info.machine_config != success:
+    #                     self.shared_info.machine_config = success
+    #             else:
+    #                 self.logger.error("平台信息同步失败")
 
     def push_task(self):
         while True:
@@ -91,6 +91,4 @@ class Agent:
                 else:
                     if success is False:
                         self.logger.error("流量信息推送失败")
-            if not self.push_event.is_set():
-                self.push_event.set()  # 第一次任务完成后设置事件
             time.sleep(self.push_interval)
