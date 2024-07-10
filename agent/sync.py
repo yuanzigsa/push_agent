00625f8487f98b8664ca66a15ea173c3dc2a84f9
yzgsa <8397701+yzgsa@user.noreply.gitee.com>
2024-07-10 12:45:22 +0800

    test

--- agent/sync.py
+++ agent/sync.py
@@ -176,18 +176,18 @@ class ServerSync:
 
         # if len(info) != cycle_times:
         #     self.logger.info("数据无效，无法进行推送")
-        #     self.update_history(device_name, "faild", playload, current_time['formatted_time'])
+        #     self.update_history(device_name, "faild", ','.join([str(item) for item in values]), current_time['formatted_time'])
         #     send_dingtalk_message(f"{device_name}推送失败", "url")
         #     return False
 
         success = self.push_to_costumer(global_config, playload, headers)
         if success:
             # 更新历史记录
-            self.update_history(device_name, "success", playload, current_time['formatted_time'])
+            self.update_history(device_name, "success", ','.join([str(item) for item in values]), current_time['formatted_time'])
             return True
         else:
             # 更新历史记录，钉钉告警
-            self.update_history(device_name, "faild", playload, current_time['formatted_time'])
+            self.update_history(device_name, "faild", ','.join([str(item) for item in values]), current_time['formatted_time'])
             send_dingtalk_message(f"{device_name}推送失败", "url")
             return False
 
