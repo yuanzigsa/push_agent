2a37931e1a0f4d3fd4f392d00bb7943932a29deb
yzgsa <yuanzgsa@gmail.com>
2024-07-12 14:50:33 +0800

    优化部署脚本

--- scripts/init.sh
+++ scripts/init.sh
@@ -28,20 +28,6 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# 检查服务状态
-check_push_agent_service() {
-    status=$(service push_agent status > /dev/null 2>&1 && echo "active" || echo "inactive")
-    if [ "$status" = "active" ]; then
-        log_info "检测到push_agent已经处于Active状态。\n"
-        service push_agent status -l
-        read -p "是否清除之前的所有部署，重新进行部署？ (y/n) 等待10s后退出: " answer
-        log_info "重新部署操作已被取消。"
-        exit 1
-        return 1
-    else
-        return 0
-    fi
-}
 
 # 配置dns
 check_configure_dns() {
@@ -64,16 +50,18 @@ check_time() {
 
 # python3环境部署及所需外置库的安装
 install_python3_env() {
+#    curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo &> /dev/null
+#    log_info "已切换到阿里云yum源"
+    # 安装yum源插件
+    yum install yum-fastestmirror -y &> /dev/null
+    log_info "已安装yum源自动选择插件，会自动优先选择最快的yum源"
+
     log_info "开始安装python3..."
     sudo yum install -y python3 &> /dev/null
     log_info "python3已安装"
 
     log_info "开始安装gcc..."
     sudo yum install -y gcc python3-devel &> /dev/null
-    log_info "gcc已安装"
-
-    log_info "开始安装git..."
-    sudo yum install -y git &> /dev/null
     log_info "gcc已安装"
 
     log_info "开始安装python所需的外置库..."
@@ -96,72 +84,54 @@ create_systemd_service() {
     systemctl start push_agent.service &> /dev/null
     log_info "PushAgent程序已创建并写进系统服务并设置成开机自启"
 }
-check_log() {
-    log_file="/opt/push_agent/log/pushAgent.log"
-    search_string="平台信息同步成功"
-    timeout=600  # 设置超时时间为600秒
-    elapsed_time=0
-
-    while [ $elapsed_time -lt $timeout ]; do
-        # 检查日志文件中是否包含特定字符
-        if grep -q "$search_string" "$log_file"; then
-            sleep 10
-            log_info "执行结束！"
-            break
-        fi
-        sleep 1
-        ((elapsed_time++))
-    done
-    sleep 1
-    if [ $elapsed_time -ge $timeout ]; then
-        log_error "超时退出...请检查！"
-    fi
-}
 
 # 部署push_agent程序
 deploy_push_agent() {
+    log_info "开始安装git..."
+    sudo yum install -y git &> /dev/null
+    log_info "gcc已安装"
+
     # 下载agent
     mkdir -p /opt/
-    cd /opt/ && git clone $PROJECT_URL
-    log_info "push_agent监管程序下载完成"
-    # 解压
-    cd /opt/push_agent/
+    path="/opt/push_agent"
+
+    if [ -e "$path" ]; then
+        log_info "PushAgent程序已存在，正在更新..."
+        cd /opt/push_agent
+        git stash save --include-untracked && git stash drop && git pull
+        service push_agent restart
+        log_info "PushAgent程序已经更新重启！"
+
+    else
+        log_warning "PushAgent程序不存在，正在部署..."
+        cd /opt/ && git clone $PROJECT_URL
+        log_info "PushAgent程序下载完成"
+    fi
+    mkdir -p /opt/push_agent/info
+    mkdir -p /opt/push_agent/log
+
 
     # 写入machineTag到系统内
     echo "$SPUG_HOST_ID:$SPUG_HOST_HOSTNAME" > /opt/push_agent/info/machineTag.info
     log_info "machineTag已写入系统内"
 
     # 写入access_token
-    echo "c?JQbPWjrz^vyCn{[W(su>@y$,,,**," > /opt/push_agent/info/access_token
+    echo ACCESS_TOKEN > /opt/push_agent/info/access_token
     log_info "access_token已写入系统内"
-
-
-    # 安装yum源插件
-    yum install yum-fastestmirror -y &> /dev/null
-    log_info "已安装yum源自动选择插件，会自动优先选择最快的yum源"
 
     # 安装基础环境
     install_python3_env
-
-    # 启动服务并检查日志
-    rm -rf /opt/push_agent/log/pushAgent.log
-    touch /opt/push_agent/log/pushAgent.log
     create_systemd_service
-    tail -f /opt/push_agent/log/pushAgent.log &
-    TAIL_PID=$!
-    check_log
-    kill $TAIL_PID
 }
+
+PROJECT_URL="https://gitee.com/yzgsa/push_agent.git"
+ACCESS_TOKEN="c?JQbPWjrz^vyCn{[W(su>@y$,,,**,"
 
 # 配置dns确保正确解析域名
 check_configure_dns
 # 校准时间时区
 check_time
-
-# 部署AutoPCDN程序(如果未部署)
-PROJECT_URL="https://gitee.com/yuanzigsa/push_agent.git"
-if check_push_agent_service; then
-    deploy_push_agent
-fi
-
-
+# 部署AutoPCDN程序
+deploy_push_agent
+sleep 2
+service push_agent status
